b89517403f907e825746ca464fa0eb6b9ddbb0cf
Benjamin Riggs <deisum@gmail.com>
2018-10-05 18:43:03 -0500

    Fugly but working.

--- AMMDK/bootstrap_software.sh
+++ AMMDK/bootstrap_software.sh
@@ -2,8 +2,10 @@
 # This script will install the current stable AMMDK software stack on an installation of AMMDK Linux
 
 echo "Installing dependencies"
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt update -y
-sudo apt install -y openocd cmake default-jdk gradle libboost-all-dev libtinyxml2-dev gcc-arm-none-eabi rapidjson-dev doxygen sqlite3 libsqlite3-dev sqlite libsqlite-dev
+sudo apt install -y openocd cmake default-jdk gradle libboost-all-dev libtinyxml2-dev gcc-arm-none-eabi rapidjson-dev doxygen sqlite3 libsqlite3-dev sqlite libsqlite-dev man-db git-lfs
+git lfs install
 
 echo "Downloading K66 code"
 pushd ~/
@@ -16,17 +18,32 @@ echo "Flashing K66"
 ./flash.sh debug/amm-tiny.elf
 popd
 
+# FIXME: Installing Biogears twice should NOT be necessary
 echo "Downloading Biogears"
 scp draum@smf.vcom3d.com:/home/draum/usr_aarch64.tar.bz2 .
+git clone https://github.com/EntropicEngineering/biogears_aarch64.git
+pushd biogears_aarch64
 echo "Installing Biogears"
+sudo tar xzf biogears.tar.gz -C /opt
+sudo chown -R linaro:amm /opt/biogears
+popd
 sudo tar xjf usr_aarch64.tar.bz2 --strip-components=1 -C /usr
+
+echo "Installing Biogears"
 
 echo "Installing Fast-RTPS"
 git clone https://github.com/eProsima/Fast-RTPS.git
-pushd Fast-RTPS
-mkdir build && pushd build
+mkdir Fast-RTPS/build && pushd Fast-RTPS/build
 cmake -DBUILD_JAVA=ON -DTHIRDPARTY=ON ..
 sudo make # Because Fast-RTPS has a dirty repo
+sudo make install
+popd
+
+echo "Installing Pistache"
+git clone --recursive https://github.com/oktal/pistache.git
+mkdir pistache/build && pushd pistache/build
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+make
 sudo make install
 popd
 
