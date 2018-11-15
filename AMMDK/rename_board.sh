2c224f550534b5abac8e42fdf153c70deaa605af
Benjamin Riggs <deisum@gmail.com>
2018-11-15 16:30:22 -0600

    Now using more better `hostnamectl` command

--- AMMDK/rename_board.sh
+++ AMMDK/rename_board.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # This script will change the hostname of the OS
 
-HOSTNAME_FILE=/etc/hostname
 HOSTS_FILE=/etc/hosts
-HOSTNAME_SCRIPT=/etc/init.d/hostname.sh
 OLD_HOSTNAME=`cat /etc/hostname`
 if [[ $# -eq 0 || $1 == -* ]]; then
     echo "Specify desired new name. e.g. AMM-Core, AMM-Torso, or AMM-Fluidics"
@@ -13,8 +11,7 @@ else
         exit
     fi
     NEW_HOSTNAME=$1
-    sed -i "s/${OLD_HOSTNAME}/${NEW_HOSTNAME}/g" ${HOSTNAME_FILE}
+    hostnamectl set-hostname ${NEW_HOSTNAME}
     sed -i "s/${OLD_HOSTNAME}/${NEW_HOSTNAME}/g" ${HOSTS_FILE}
-    ${HOSTNAME_SCRIPT} start
     echo "Renamed board from ${OLD_HOSTNAME} to ${NEW_HOSTNAME}"
 fi
