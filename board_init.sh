5cdc19497564dc2569a2bdea4370ff70d694fdd6
Benjamin Riggs <deisum@gmail.com>
2018-11-20 19:05:51 -0600

    If necessary, switch user to 'amm' before starting.

--- board_init.sh
+++ board_init.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 # This script will install the current stable AMMDK software stack on an installation of AMMDK Linux
 
-git clone https://github.com/AdvancedModularManikin/development-kit.git
-if [ $? -ne 0 ]; then
-    exit
+check()
+{
+    if [ $? -ne 0 ]; then
+        exit
+    fi
+}
+
+if [ $USER != 'amm' ]; then
+    su amm
+    check
 fi
+git clone https://github.com/AdvancedModularManikin/development-kit.git
+check
 ./development-kit/AMMDK/bootstrap_software.sh
 
