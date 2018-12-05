41f1866d9ea9475d5891c31b9b459a5d96f3385c
Benjamin Riggs <deisum@gmail.com>
2018-12-05 15:26:36 -0600

    Adjust blink rate

--- AMMDK/ammdk_tester/gpio_led_output.c
+++ AMMDK/ammdk_tester/gpio_led_output.c
@@ -64,7 +64,7 @@ void delay(void);
 void delay(void)
 {
     volatile uint32_t i = 0;
-    for (i = 0; i < 800000; ++i)
+    for (i = 0; i < 4000000; ++i) // ~ 1 Hz
     {
         __asm("NOP"); /* delay */
     }
