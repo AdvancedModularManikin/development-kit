1074eb7410e1c45a4b8bab6690a50f58644003b0
MP <matthew.pang@gmail.com>
2019-03-05 16:13:31 -0600

    Update bootstrap_embedded_only.sh

--- AMMDK/bootstrap_embedded_only.sh
+++ AMMDK/bootstrap_embedded_only.sh
@@ -12,11 +12,8 @@ echo "Installing dependencies"
 sudo apt update -y
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y openocd cmake default-jdk gradle gdb-multiarch tmux libboost-all-dev libtinyxml2-dev gcc-arm-none-eabi rapidjson-dev doxygen sqlite3 libsqlite3-dev sqlite libsqlite-dev man-db nginx supervisor
+sudo apt install -y openocd cmake gdb-multiarch tmux gcc-arm-none-eabi man-db
 check
-
-pushd ~/
-ln -s development-kit/AMMDK/rename_board.sh .
 
 echo "Downloading K66 code"
 git clone --recursive https://github.com/AdvancedModularManikin/amm-tiny.git
