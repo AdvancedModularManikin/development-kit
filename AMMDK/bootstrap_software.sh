01950f8d10b48e6bd940a39b9704ca0949abf4c3
MP <matthew.pang@gmail.com>
2019-03-12 17:09:30 -0500

    Update bootstrap_software.sh

--- AMMDK/bootstrap_software.sh
+++ AMMDK/bootstrap_software.sh
@@ -9,10 +9,11 @@ check()
 }
 
 echo "Installing dependencies"
+sudo apt-mark hold firmware-atheros
 sudo apt update -y
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y openocd cmake default-jdk gradle gdb-multiarch tmux libboost-all-dev libtinyxml2-dev gcc-arm-none-eabi rapidjson-dev doxygen sqlite3 libsqlite3-dev sqlite libsqlite-dev man-db nginx supervisor
+sudo apt install -y openocd cmake default-jdk xrdp tigervnc-standalone-server gradle gdb-multiarch tmux libboost-all-dev libtinyxml2-dev gcc-arm-none-eabi rapidjson-dev doxygen sqlite3 libsqlite3-dev sqlite libsqlite-dev man-db nginx supervisor
 check
 
 pushd ~/
