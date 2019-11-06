48d3ce6804c104ec7dc23f107416a6b541f9740f
Benjamin Riggs <deisum@gmail.com>
2019-11-06 17:31:27 -0600

    Commenting out old stuff. Testing the rest.

--- software/bootstrap_software.sh
+++ software/bootstrap_software.sh
@@ -39,62 +39,62 @@ echo "Flashing K66"
 check
 popd
 
-# FIXME: Installing Biogears twice should NOT be necessary
-echo "Downloading Biogears"
-scp draum@smf.vcom3d.com:/home/draum/usr_aarch64.tar.bz2 .
-check
-echo "Installing Biogears"
-sudo tar xjf usr_aarch64.tar.bz2 --strip-components=1 -C /usr
-check
-
-echo "Installing Fast-RTPS"
-git clone https://github.com/eProsima/Fast-RTPS.git
-check
-mkdir Fast-RTPS/build && pushd Fast-RTPS/build
-cmake -DBUILD_JAVA=ON -DTHIRDPARTY=ON ..
-check
-sudo make # Because Fast-RTPS has a dirty repo
-check
-sudo make install
-check
-popd
-
-echo "Installing Pistache"
-git clone --recursive https://github.com/oktal/pistache.git
-check
-mkdir pistache/build && pushd pistache/build
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-check
-make
-check
-sudo make install
-check
-popd
-
-echo "Downloading Application code"
-git clone --recursive --branch develop https://github.com/AdvancedModularManikin/DDS.git
-check
-mkdir DDS/AMM_Modules/build && pushd DDS/AMM_Modules/build
-echo "Building Applications"
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
-check
-make
-check
-popd
-
-echo "Installing Web Interface"
-scp draum@smf.vcom3d.com:/home/draum/amm_instructor.tar.bz2 .
-check
-sudo rm -rf /var/www/html/*
-check
-sudo tar jxf amm_instructor.tar.bz2 -C /var/www/html/
-check
-ln -s DDS/AMM_Modules/build/bin/ AMM
-check
-sudo cp DDS/AMM_Modules/support/supervisor_scripts/*.conf /etc/supervisor/conf.d/
-check
-sudo systemctl restart supervisor
-check
+## FIXME: Installing Biogears twice should NOT be necessary
+#echo "Downloading Biogears"
+#scp draum@smf.vcom3d.com:/home/draum/usr_aarch64.tar.bz2 .
+#check
+#echo "Installing Biogears"
+#sudo tar xjf usr_aarch64.tar.bz2 --strip-components=1 -C /usr
+#check
+#
+#echo "Installing Fast-RTPS"
+#git clone https://github.com/eProsima/Fast-RTPS.git
+#check
+#mkdir Fast-RTPS/build && pushd Fast-RTPS/build
+#cmake -DBUILD_JAVA=ON -DTHIRDPARTY=ON ..
+#check
+#sudo make # Because Fast-RTPS has a dirty repo
+#check
+#sudo make install
+#check
+#popd
+#
+#echo "Installing Pistache"
+#git clone --recursive https://github.com/oktal/pistache.git
+#check
+#mkdir pistache/build && pushd pistache/build
+#cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+#check
+#make
+#check
+#sudo make install
+#check
+#popd
+#
+#echo "Downloading Application code"
+#git clone --recursive --branch develop https://github.com/AdvancedModularManikin/DDS.git
+#check
+#mkdir DDS/AMM_Modules/build && pushd DDS/AMM_Modules/build
+#echo "Building Applications"
+#cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release ..
+#check
+#make
+#check
+#popd
+#
+#echo "Installing Web Interface"
+#scp draum@smf.vcom3d.com:/home/draum/amm_instructor.tar.bz2 .
+#check
+#sudo rm -rf /var/www/html/*
+#check
+#sudo tar jxf amm_instructor.tar.bz2 -C /var/www/html/
+#check
+#ln -s DDS/AMM_Modules/build/bin/ AMM
+#check
+#sudo cp DDS/AMM_Modules/support/supervisor_scripts/*.conf /etc/supervisor/conf.d/
+#check
+#sudo systemctl restart supervisor
+#check
 
 popd
 echo "Setup Completed"
