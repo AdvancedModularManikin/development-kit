44dec67e87c4882124fccf501861f57f27f1558f
Benjamin Riggs <deisum@gmail.com>
2019-11-06 18:52:12 -0600

    Add handy note

--- board_init.sh
+++ board_init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # This script will install the current stable AMMDK software stack on an installation of AMMDK Linux
 
+# Run me via: $ bash <(curl -s https://raw.githubusercontent.com/AdvancedModularManikin/development-kit/master/board_init.sh)
+
 check()
 {
     if [ $? -ne 0 ]; then
